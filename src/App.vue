97d93d19c6b37e65c54aeda64294ece94167a694
zhoushaohang <1181842363@qq.com>
2021-09-27 18:16:36 +0800

    引入and

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <a-menu v-model:selectedKeys="current" mode="horizontal">
+    <a-menu-item key="mail">
+      <template #icon>
+        <mail-outlined />
+      </template>
+      Navigation One
+    </a-menu-item>
+    <a-menu-item key="app" >
+      <template #icon>
+        <appstore-outlined />
+      </template>
+      Navigation Two
+    </a-menu-item>
+    <a-sub-menu>
+      <template #icon>
+        <setting-outlined />
+      </template>
+      <template #title>Navigation Three - Submenu</template>
+      <a-menu-item-group title="Item 1">
+        <a-menu-item key="setting:1">Option 1</a-menu-item>
+        <a-menu-item key="setting:2">Option 2</a-menu-item>
+      </a-menu-item-group>
+      <a-menu-item-group title="Item 2">
+        <a-menu-item key="setting:3">Option 3</a-menu-item>
+        <a-menu-item key="setting:4">Option 4</a-menu-item>
+      </a-menu-item-group>
+    </a-sub-menu>
+    <a-menu-item key="alipay">
+      <a href="https://antdv.com" target="_blank" rel="noopener noreferrer">
+        Navigation Four - Link
+      </a>
+    </a-menu-item>
+  </a-menu>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent, ref } from 'vue';
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons-vue';
+export default defineComponent({
+  setup() {
+    const current = ref(['mail']);
+    return {
+      current,
+    };
+  },
 
-export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    MailOutlined,
+    AppstoreOutlined,
+    SettingOutlined,
+  },
+});
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
