f384404631cb41042b3da6632aef8074b1d8a450
zhoushaohang <1181842363@qq.com>
2021-10-11 15:04:56 +0800

    bug更改

--- src/views/kejifuwu/2daizhijianku.vue
+++ src/views/kejifuwu/2daizhijianku.vue
@@ -3,7 +3,7 @@
 </template>
 <script>
 export default {
-  name: '',
+  name: '2d',
   props: {},
   components: {},
   data() {
--- src/views/kejifuwu/boozhijianku.vue
+++ src/views/kejifuwu/boozhijianku.vue
@@ -4,7 +4,7 @@
 </template>
 <script>
 export default {
-  name: '',
+  name: 'bo',
   props: {},
   components: {},
   data() {
